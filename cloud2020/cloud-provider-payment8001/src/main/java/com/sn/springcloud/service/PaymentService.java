0600e6fca11d8fd48c1c58dc4cdad5de0f518a29
shen.ning <shen.ning@rongzer.com>
2020-05-25 08:41:30 +0800

    初次提交

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/sn/springcloud/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.sn.springcloud.service;
+
+import com.sn.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    public int create(Payment payment);
+    public Payment getPaymentById(@Param("id")Long id);
+}
---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/sn/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,22 @@
+package com.sn.springcloud.service.impl;
+
+import com.sn.springcloud.dao.PaymentDao;
+import com.sn.springcloud.entities.Payment;
+import com.sn.springcloud.service.PaymentService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+    public int create(Payment payment){
+        return  paymentDao.create(payment);
+    }
+    public Payment getPaymentById(Long id){
+        return paymentDao.getPaymentById(id);
+    }
+}
